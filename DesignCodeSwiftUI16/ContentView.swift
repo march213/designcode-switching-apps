ed9c6138cfef72aab328679768ce18ed670fa8ce
march213 <jane.molodetskaya@gmail.com>
2024-07-14 20:50:34 -0400

    customize content view

--- DesignCodeSwiftUI16/ContentView.swift
+++ DesignCodeSwiftUI16/ContentView.swift
@@ -9,29 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      VStack(spacing: 20.0) {
-            Image(systemName: "timelapse")
+        VStack(spacing: 20.0) {
+            Image(systemName: "timelapse", variableValue: 0.2)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-          Text("Switching Apps")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-          Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
-            .font(.body)
-            .fontWeight(.regular)
-            .multilineTextAlignment(.center)
-          Button("Got It") {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-          }
-          .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-          .frame(width: 300.0)
-          .border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 50.0))
+                .fontWeight(.thin)
+            Text("Switching Apps")
+              .font(.largeTitle)
+              .fontWeight(.bold)
+            Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
+              .font(.body)
+              .fontWeight(.regular)
+              .multilineTextAlignment(.center)
+            Button {
+                // Action
+            } label: {
+                Text("Got it")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .stroke()
+                    )
+            }
+            .accentColor(.primary)
+          
         }
-        
+        .padding(30)
+        .background(.ultraThinMaterial)
+        .cornerRadius(10)
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke())
+        .padding(10)
     }
 }
 
 #Preview {
     ContentView()
+        .background(Image("Wallpaper 2"))
 }
